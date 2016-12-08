783a4eb3d0a3f6844456e742a85f0bea274922f8
Ben Goodwyn <ben@clouddirective.com>
2016-12-07 21:09:25 -0600

    AoC 2016 Day 3 Part 2

--- 2016-03/main.cpp
+++ 2016-03/main.cpp
@@ -9,13 +9,21 @@ int main() {
 	std::ifstream input("input");
 	std::string line;
 	int valid = 0;
+	int y = 0;
 	while (std::getline(input, line)) {
 		std::stringstream stream(line);
-		std::array<int, 3> sides;
-		stream >> sides[0] >> sides[1] >> sides[2];
-		std::sort(sides.begin(), sides.end());
-		if (sides[0] + sides[1] > sides[2]) {
-			++valid;
+		std::array<std::array<int, 3>, 3> sides;
+		stream >> sides[0][y] >> sides[1][y] >> sides[2][y];
+		if (2 == y) {
+			for (auto column = 0; column < 3; ++column) {
+				std::sort(sides[column].begin(), sides[column].end());
+				if (sides[column][0] + sides[column][1] > sides[column][2]) {
+					++valid;
+				}
+			}
+			y = 0;
+		} else {
+			++y;
 		}
 	}
 	std::cout << valid << std::endl;
