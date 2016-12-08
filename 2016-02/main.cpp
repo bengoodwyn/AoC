282053b37b20481d6fd28491661ce80258427fc9
Ben Goodwyn <ben@clouddirective.com>
2016-12-07 20:49:57 -0600

    Refactor to get ready for part 2

--- 2016-02/main.cpp
+++ 2016-02/main.cpp
@@ -6,21 +6,20 @@
 
 class Key {
 public:
-	const int digit;
-	const int up;
-	const int left;
-	const int right;
-	const int down;
+	char up;
+	char left;
+	char right;
+	char down;
 
-	Key(int digit
-		, int up
-		, int left
-		, int right
-		, int down)
-		: digit(digit), up(up), left(left), right(right), down(down) {
+	Key()
+		: up(0), left(0), right(0), down(0) {
 	}
 
-	int move(char direction) const {
+	Key(char up, char left, char right, char down)
+		: up(up), left(left), right(right), down(down) {
+	}
+
+	char move(char direction) const {
 		switch (direction) {
 			case 'U':
 				return up;
@@ -39,60 +38,46 @@ public:
 	}
 };
 
-const std::array<Key
-	, 10> keys = {Key(0
-	, 0
-	, 0
-	, 0
-	, 0), Key(1
-	, 1
-	, 1
-	, 2
-	, 4), Key(2
-	, 2
-	, 1
-	, 3
-	, 5), Key(3
-	, 3
-	, 2
-	, 3
-	, 6), Key(4
-	, 1
-	, 4
-	, 5
-	, 7), Key(5
-	, 2
-	, 4
-	, 6
-	, 8), Key(6
-	, 3
-	, 5
-	, 6
-	, 9), Key(7
-	, 4
-	, 7
-	, 8
-	, 7), Key(8
-	, 5
-	, 7
-	, 9
-	, 8), Key(9
-	, 6
-	, 8
-	, 9
-	, 9)};
+class Keypad {
+public:
+	Keypad()
+		: currentKey('5') {
+		keys['0'] = Key('0', '0', '0', '0');
+		keys['1'] = Key('1', '1', '2', '4');
+		keys['2'] = Key('2', '1', '3', '5');
+		keys['3'] = Key('3', '2', '3', '6');
+		keys['4'] = Key('1', '4', '5', '7');
+		keys['5'] = Key('2', '4', '6', '8');
+		keys['6'] = Key('3', '5', '6', '9');
+		keys['7'] = Key('4', '7', '8', '7');
+		keys['8'] = Key('5', '7', '9', '8');
+		keys['9'] = Key('6', '8', '9', '9');
+	};
 
-int currentKey = 5;
+	char move(char direction) {
+		currentKey = keys[currentKey].move(direction);
+		return currentKey;
+	}
+
+	char current() const {
+		return currentKey;
+	}
+
+private:
+	std::array<Key, 256> keys;
+	char currentKey;
+};
+
 
 int main() {
 	std::ifstream input("input");
 	std::string line;
-	while (std::getline(input
-		, line)) {
+	Keypad keypad;
+	while (std::getline(input, line)) {
 		for (auto move : line) {
-			currentKey = keys[currentKey].move(move);
+			keypad.move(move);
 		}
-		std::cout << currentKey << std::endl;
+		std::cout << keypad.current() << std::endl;
 	}
 	return 0;
 }
