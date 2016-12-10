0d4a134bb50514d59d8cf53a292bfe82501cd2b4
Ben Goodwyn <ben@clouddirective.com>
2016-12-09 20:17:04 -0600

    AoC 2016 Day 5 Part 2

--- 2016-05/main.cpp
+++ 2016-05/main.cpp
@@ -12,20 +12,29 @@
 constexpr std::uint8_t PASSWORD_LENGTH = 8;
 
 std::string calculatePassword(std::string key) {
-	std::stringstream password;
+	std::string password(PASSWORD_LENGTH, '*');
 	std::uint64_t salt = 0;
-	while (true) {
+	int remaining = PASSWORD_LENGTH;
+	std::cout << password << std::endl;
+	while (0 < remaining) {
 		std::string saltedKey = key + std::to_string(salt);
 		unsigned char digest[16];
 		MD5(reinterpret_cast<const unsigned char*>(saltedKey.data()), saltedKey.length(), digest);
 		if (0 == (digest[0] | digest[1] | (digest[2] >> 4))) {
-			password << std::hex << (digest[2] & 0x0FU);
-			if (password.str().length() == PASSWORD_LENGTH) {
-				return password.str();
+			uint8_t index = digest[2] & 0x0FU;
+			if (index < PASSWORD_LENGTH) {
+				if ('*' == password.at(index)) {
+					std::stringstream str;
+					str << std::hex << (digest[3] >> 4);
+					password.at(index) = str.str().at(0);
+					--remaining;
+					std::cout << password << std::endl;
+				}
 			}
 		}
 		++salt;
 	}
+	return password;
 }
 
 int main(int argc, const char* argv[]) {
