e0b475779600401a4289eaf76f74f6165c2327ee
Ben Goodwyn <ben@clouddirective.com>
2016-12-10 12:27:44 -0600

    AoC 2016 Day 9 Part 1 but don't store all in memory

--- 2016-09/main.cpp
+++ 2016-09/main.cpp
@@ -6,9 +6,13 @@
 int main(int argc, const char* argv[]) {
 	std::ifstream input("input");
 	std::stringstream output;
+	int decompressedLength = 0;
 	Decompressor decompressor;
 	while (decompressor.run(input, output)) {
+		decompressedLength += output.str().length();
+		output.str(std::string());
 	}
-	std::cout << output.str().length() << std::endl;
+	decompressedLength += output.str().length();
+	std::cout << decompressedLength << std::endl;
 	return 0;
 }
