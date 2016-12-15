96bc38bd5de9b4d7f4aa6218cc63a4794fa916eb
Ben Goodwyn <ben@clouddirective.com>
2016-12-15 17:29:15 -0600

    AoC 2016 Day 10 Unit Test Rebuild

--- 2016-10/MicrochipFactoryTests.cpp
+++ 2016-10/MicrochipFactoryTests.cpp
@@ -28,3 +28,25 @@ TEST_F(MicrochipFactoryTest, CanReproduceItemsSentToOutputBins) {
     EXPECT_EQ(100, factory->reproduceOutput(2));
     EXPECT_EQ(200, factory->reproduceOutput(3));
 }
+
+TEST_F(MicrochipFactoryTest, CanReproduceItemsSentToOutputBinsInReverse) {
+    factory->execute("bot 1 output 2 output 3");
+    factory->execute("value 100 bot 1");
+    factory->execute("value 200 bot 1");
+    factory->execute("value 3000 bot 2");
+    factory->execute("value 4000 bot 3");
+    EXPECT_EQ(100, factory->reproduceOutput(2));
+    EXPECT_EQ(200, factory->reproduceOutput(3));
+}
+
+TEST_F(MicrochipFactoryTest, CanConnectBotsToOtherBots) {
+    factory->execute("bot 3 output 3 output 30");
+    factory->execute("value 100 bot 1");
+    factory->execute("bot 2 output 2 output 20");
+    factory->execute("value 200 bot 1");
+    factory->execute("bot 1 bot 2 bot 3");
+    factory->execute("value 101 bot 1");
+    factory->execute("value 201 bot 1");
+    EXPECT_EQ(100, factory->reproduceOutput(2));
+    EXPECT_EQ(200, factory->reproduceOutput(3));
+}
