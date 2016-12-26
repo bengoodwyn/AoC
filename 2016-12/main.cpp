5f51b1072d8079bac348e348556896268b896bea
Ben Goodwyn <ben@clouddirective.com>
2016-12-26 17:24:03 -0600

    AoC 2016 Day 12 Template

---
+++ 2016-12/main.cpp
@@ -0,0 +1,11 @@
+#include <fstream>
+#include "Vm.h"
+
+using namespace AoC;
+
+int main(int argc, const char* argv[]) {
+    //    bool part2 = (argc > 1 && argv[1][0] == '2');
+    std::ifstream input("2016-12/input");
+    Vm vm;
+    return 0;
+}
---
+++ 2016-12/tests.cpp
@@ -0,0 +1,21 @@
+#include <memory>
+#include <gtest/gtest.h>
+#include "Vm.h"
+
+using namespace AoC;
+
+class VmTest : public ::testing::Test {
+public:
+    virtual void SetUp() override {
+        vm.reset(new Vm);
+    }
+
+    virtual void TearDown() override {
+        vm.reset();
+    }
+
+    std::unique_ptr<Vm> vm;
+};
+
+TEST_F(VmTest, Noop) {
+}
