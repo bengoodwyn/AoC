7eea4cf19cb2b10b73eb3423c4387841b05221d5
Ben Goodwyn <ben@clouddirective.com>
2016-12-26 18:24:46 -0600

    AoC 2016 Day 12 main compiles

--- 2016-12/main.cpp
+++ 2016-12/main.cpp
@@ -6,7 +6,7 @@ using namespace AoC;
 
 int main(int argc, const char* argv[]) {
     //    bool part2 = (argc > 1 && argv[1][0] == '2');
-    std::ifstream input("2016-12/input");
+    std::ifstream input("input");
     Vm vm;
     vm.load(input);
     vm.execute();
