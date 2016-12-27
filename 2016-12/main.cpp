476b4cdff151b912a171b06e2a4989ee37863193
Ben Goodwyn <ben@clouddirective.com>
2016-12-26 18:42:22 -0600

    AoC 2016 Day 12 Part 2

--- 2016-12/main.cpp
+++ 2016-12/main.cpp
@@ -5,9 +5,13 @@
 using namespace AoC;
 
 int main(int argc, const char* argv[]) {
-    //    bool part2 = (argc > 1 && argv[1][0] == '2');
+    bool part2 = (argc > 1 && argv[1][0] == '2');
     std::ifstream input("input");
     Vm vm;
+    if (part2) {
+        std::stringstream stream("cpy 1 c");
+        vm.load(stream);
+    }
     vm.load(input);
     vm.execute();
     std::cout << vm.read('a') << std::endl;
