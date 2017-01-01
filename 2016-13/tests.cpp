64e0ff57320369ac1b6d4bd99d3e43d0a6f9d337
Ben Goodwyn <ben@clouddirective.com>
2017-01-01 13:25:12 -0600

    AoC 2016 Day 13 TDD

--- 2016-13/tests.cpp
+++ 2016-13/tests.cpp
@@ -15,7 +15,7 @@ namespace AoC {
         }
 
         bool isWall(Point point) const {
-            return false;
+            return wallAlgorithm(point);
         }
 
         void explore(int distance) {
@@ -64,4 +64,20 @@ TEST_F(MazeTest, DistanceToNeighborOfOriginIs1) {
 
 TEST_F(MazeTest, CanExploreGivenDistanceFromOrigin) {
     ASSERT_NO_THROW(maze->explore(1));
+}
+
+TEST_F(MazeTest, AskingIsWallGetsResultFromAlgorithm) {
+    Maze::Point calledWith;
+    bool nextResult = false;
+    Maze::WallAlgorithm algo = [&calledWith, &nextResult](Maze::Point point) -> bool {
+        calledWith = point;
+        return nextResult;
+    };
+    maze.reset(new Maze(algo));
+    nextResult = true;
+    ASSERT_TRUE(maze->isWall(Maze::Point(17, 14)));
+    ASSERT_EQ(Maze::Point(17, 14), calledWith);
+    nextResult = false;
+    ASSERT_FALSE(maze->isWall(Maze::Point(97, 99)));
+    ASSERT_EQ(Maze::Point(97, 99), calledWith);
 }
