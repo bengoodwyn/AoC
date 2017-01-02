b1fc96827e748293911c5263cb18eacc8ba0826f
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 17:28:44 -0600

    AoC 2016 Day 20 TDD

--- 2016-20/tests.cpp
+++ 2016-20/tests.cpp
@@ -3,37 +3,34 @@
 #include <gtest/gtest.h>
 
 #include <tuple>
-#include <vector>
+#include <map>
 
 namespace AoC {
     class FirewallRules {
     public:
-        using Rule = std::pair<uint64_t, uint64_t>;
-
         void addRule(uint64_t start, uint64_t end) {
-            rules.push_back({start, end});
+            auto iter = rules.find(start);
+            if (rules.end() != iter) {
+                iter->second = std::max(iter->second, end);
+            } else {
+                rules.insert({start, end});
+            }
         }
 
         uint64_t lowestUnblockedAddress() {
             uint64_t address = 0;
-            while (true) {
-                bool blocked = false;
-                for (auto rule : rules) {
-                    if (address >= rule.first && address <= rule.second) {
-                        blocked = true;
-                        break;
-                    }
-                }
-                if (blocked) {
-                    ++address;
+            for (auto rule : rules) {
+                if (address >= rule.first && address <= rule.second) {
+                    address = std::max(rule.second + 1, address);
                 } else {
                     return address;
                 }
             }
+            return address;
         }
 
     private:
-        std::vector<Rule> rules;
+        std::map<uint64_t, uint64_t> rules;
     };
 }
 
@@ -69,4 +66,16 @@ TEST_F(FirewallRulesTest, CanFindLowestUnblockedAddressWithTwoOverlappingRules) 
     firewallRules->addRule(0, 10);
     firewallRules->addRule(5, 15);
     ASSERT_EQ(16, firewallRules->lowestUnblockedAddress());
+}
+
+TEST_F(FirewallRulesTest, CanFindLowestUnblockedAddressWithTwoContiguousRules) {
+    firewallRules->addRule(0, 10);
+    firewallRules->addRule(11, 20);
+    ASSERT_EQ(21, firewallRules->lowestUnblockedAddress());
+}
+
+TEST_F(FirewallRulesTest, CanFindLowestUnblockedAddressWithTwoDisjointRules) {
+    firewallRules->addRule(0, 10);
+    firewallRules->addRule(12, 20);
+    ASSERT_EQ(11, firewallRules->lowestUnblockedAddress());
 }
