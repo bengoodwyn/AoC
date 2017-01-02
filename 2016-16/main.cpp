fd2fb7e9c9c7a357d7bc0569c5b7f266afd2d5d4
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 12:37:17 -0600

    AoC 2016 Day 16 Done

--- 2016-16/main.cpp
+++ 2016-16/main.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include "Dragon.h"
+
+using namespace AoC;
+
 int main(int argc, const char* argv[]) {
-    // bool part2 = (argc > 1 && argv[1][0] == '2');
+    bool part2 = (argc > 1 && argv[1][0] == '2');
+    std::string datum = Dragon::generate("11101000110010100", part2 ? 35651584 : 272);
+    std::string checksum = Dragon::checksum(datum);
+    std::cout << checksum << std::endl;
     return 0;
 }
--- 2016-16/tests.cpp
+++ 2016-16/tests.cpp
@@ -2,51 +2,7 @@
 #include <sstream>
 #include <gtest/gtest.h>
 
-#include <cassert>
-#include <string>
-
-namespace AoC {
-    class Dragon {
-    public:
-        static std::string expand(std::string a) {
-            std::string result = a + '0';
-            for (auto iter = a.rbegin(); a.rend() != iter; iter++) {
-                if (*iter == '0') {
-                    result += '1';
-                } else if (*iter == '1') {
-                    result += '0';
-                } else {
-                    assert(false);
-                }
-            }
-            return result;
-        }
-
-        static std::string generate(std::string initial, int length) {
-            std::string result = initial;
-            while (result.length() < length) {
-                result = expand(result);
-            }
-            return result.substr(0, length);
-        }
-
-        static std::string checksum(std::string input) {
-            std::string result;
-            for (int i = 0; i < input.length(); i += 2) {
-                if (input.at(i) == input.at(i + 1)) {
-                    result += '1';
-                } else {
-                    result += '0';
-                }
-            }
-            if (0 == (result.length() % 2)) {
-                return checksum(result);
-            } else {
-                return result;
-            }
-        }
-    };
-}
+#include "Dragon.h"
 
 using namespace AoC;
 
