bcefacf14bc294a0aebbc3f971e703d59bd93c07
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 17:30:08 -0600

    AoC 2016 Day 20 Extract

---
+++ 2016-20/FirewallRules.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdint>
+#include <map>
+
+namespace AoC {
+    class FirewallRules {
+    public:
+        void addRule(uint64_t start, uint64_t end) {
+            auto iter = rules.find(start);
+            if (rules.end() != iter) {
+                iter->second = std::max(iter->second, end);
+            } else {
+                rules.insert({start, end});
+            }
+        }
+
+        uint64_t lowestUnblockedAddress() {
+            uint64_t address = 0;
+            for (auto rule : rules) {
+                if (address >= rule.first && address <= rule.second) {
+                    address = std::max(rule.second + 1, address);
+                } else {
+                    return address;
+                }
+            }
+            return address;
+        }
+
+    private:
+        std::map<uint64_t, uint64_t> rules;
+    };
+}
