34bf946230149abaa0c26d3296f1dc67a3126f62
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 13:50:36 -0600

    AoC 2016 Day 17 Part 2

--- 2016-17/main.cpp
+++ 2016-17/main.cpp
@@ -4,7 +4,11 @@
 using namespace AoC;
 
 int main(int argc, const char* argv[]) {
-    // bool part2 = (argc > 1 && argv[1][0] == '2');
-    std::cout << ShortestPath::find("pgflpeqp") << std::endl;
+    bool part2 = (argc > 1 && argv[1][0] == '2');
+    if (part2) {
+        std::cout << ShortestPath::find("pgflpeqp", ShortestPath::PathType::Longest).length() << std::endl;
+    } else {
+        std::cout << ShortestPath::find("pgflpeqp") << std::endl;
+    }
     return 0;
 }
