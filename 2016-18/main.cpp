3f3fc4b75f14ac184e15f6e48b988b4f6e7a8205
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 14:54:34 -0600

    AoC 2016 Day 18 Part 1

--- 2016-18/main.cpp
+++ 2016-18/main.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+#include "Tiler.h"
+
+using namespace AoC;
+
 int main(int argc, const char* argv[]) {
     // bool part2 = (argc > 1 && argv[1][0] == '2');
+    const std::string inputRow
+        ("^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^");
+    const int rows = 40;
+    int safeTiles = Tiler::countSafeTiles(inputRow);
+    std::string currentRow = inputRow;
+    for (int i = 1; i < rows; ++i) {
+        currentRow = Tiler::nextRow(currentRow);
+        safeTiles += Tiler::countSafeTiles(currentRow);
+    }
+    std::cout << safeTiles << std::endl;
     return 0;
 }
