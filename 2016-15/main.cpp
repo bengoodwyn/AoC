17084ba94934c773f21e19bbfcee2431c5157c49
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 11:56:18 -0600

    AoC 2016 Day 15 Part 1

--- 2016-15/main.cpp
+++ 2016-15/main.cpp
@@ -1,4 +1,12 @@
+#include <fstream>
+#include "Sculpture.h"
+
+using namespace AoC;
+
 int main(int argc, const char* argv[]) {
     //bool part2 = (argc > 1 && argv[1][0] == '2');
+    std::ifstream input("preprocessed-input");
+    Sculpture sculpture(input);
+    std::cout << sculpture.execute() << std::endl;
     return 0;
 }
--- 2016-15/tests.cpp
+++ 2016-15/tests.cpp
@@ -2,48 +2,7 @@
 #include <sstream>
 #include <gtest/gtest.h>
 
-#include <cctype>
-#include <iostream>
-
-namespace AoC {
-    class Sculpture {
-    public:
-        Sculpture(std::istream& stream) {
-            int size, initial;
-            while (!stream.eof()) {
-                stream >> size >> initial;
-                discs.push_back({size, initial});
-                while (!stream.eof() && std::isspace(stream.peek())) {
-                    stream.get();
-                }
-            }
-        }
-
-        int execute() {
-            int dropped_at = 0;
-            while (true) {
-                if (test_disc(dropped_at)) {
-                    return dropped_at;
-                }
-                ++dropped_at;
-            }
-        }
-
-    private:
-        bool test_disc(int dropped_at) {
-            int delta_t = 0;
-            for (auto disc : discs) {
-                ++delta_t;
-                if (0 != ((dropped_at + delta_t + disc.second) % disc.first)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        std::vector<std::pair<int, int>> discs;
-    };
-}
+#include "Sculpture.h"
 
 using namespace AoC;
 
