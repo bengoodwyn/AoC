18a8d4aafb114e04da9d7d3a80156cbb7a350a3a
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 14:55:46 -0600

    AoC 2016 Day 18 Part 2

--- 2016-18/main.cpp
+++ 2016-18/main.cpp
@@ -4,10 +4,10 @@
 using namespace AoC;
 
 int main(int argc, const char* argv[]) {
-    // bool part2 = (argc > 1 && argv[1][0] == '2');
+    bool part2 = (argc > 1 && argv[1][0] == '2');
     const std::string inputRow
         ("^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^");
-    const int rows = 40;
+    const int rows = part2 ? 400000 : 40;
     int safeTiles = Tiler::countSafeTiles(inputRow);
     std::string currentRow = inputRow;
     for (int i = 1; i < rows; ++i) {
