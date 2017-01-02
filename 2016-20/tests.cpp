3fbc6d4eaa806f4456681bb023282ba0bf08c300
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 17:17:43 -0600

    AoC 2016 Day 20 TDD

--- 2016-20/tests.cpp
+++ 2016-20/tests.cpp
@@ -3,10 +3,28 @@
 #include <gtest/gtest.h>
 
 namespace AoC {
-
+    class FirewallRules {
+    public:
+        void addRule(uint64_t start, uint64_t end) {
+        }
+    };
 }
 
 using namespace AoC;
 
-TEST(FirewallRulesTest, Noop) {
+class FirewallRulesTest : public ::testing::Test {
+public:
+    virtual void SetUp() override {
+        firewallRules.reset(new FirewallRules());
+    }
+
+    virtual void TearDown() override {
+        firewallRules.reset();
+    }
+
+    std::unique_ptr<FirewallRules> firewallRules;
+};
+
+TEST_F(FirewallRulesTest, CanAddRuleToFirewall) {
+    firewallRules->addRule(0, 10);
 }
