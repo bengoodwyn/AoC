93795bd4a606362bc3196eae4a1c5b7ccbc89c20
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 10:05:14 -0600

    AoC 2016 Day 14 Construction with Salt

--- 2016-14/tests.cpp
+++ 2016-14/tests.cpp
@@ -1,14 +1,30 @@
+#include <memory>
 #include <gtest/gtest.h>
 
-class TestCase : public ::testing::Test {
+#include <string>
+
+class KeyGenerator {
+public:
+    KeyGenerator(std::string salt) : salt(salt) {
+    }
+
+private:
+    std::string salt;
+};
+
+class KeyGeneratorTests : public ::testing::Test {
 public:
     virtual void SetUp() override {
+        keyGenerator.reset(new KeyGenerator("abc"));
     }
 
     virtual void TearDown() override {
+        keyGenerator.reset();
     }
+
+    std::unique_ptr<KeyGenerator> keyGenerator;
 };
 
-TEST_F(TestCase, Template) {
+TEST_F(KeyGeneratorTests, Template) {
     ASSERT_TRUE(true);
 }
