9bed485a2355071c5927fbe99c2175bdbe4f49a6
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 21:50:08 -0600

    AoC 2016 Day 22 TDD

--- 2016-22/tests.cpp
+++ 2016-22/tests.cpp
@@ -3,15 +3,33 @@
 #include <gtest/gtest.h>
 
 #include <tuple>
+#include <map>
 
 namespace AoC {
     class Grid {
     public:
         using Id = std::pair<int, int>;
+        using DiskUsage = std::pair<unsigned int, unsigned int>;
 
         void addNode(Id id, int used, int free) {
-
+            grid[id] = {used, free};
         }
+
+        int countViablePairs() {
+            int count = 0;
+            for (auto sourceNode : grid) {
+                for (auto targetNode : grid) {
+                    if ((sourceNode.second.first > 0) && (sourceNode.first != targetNode.first) &&
+                        (sourceNode.second.first <= targetNode.second.second)) {
+                        ++count;
+                    }
+                }
+            }
+            return count;
+        }
+
+    private:
+        std::map<Id, DiskUsage> grid;
     };
 }
 
@@ -32,4 +50,21 @@ public:
 
 TEST_F(GridTest, CanAddANodeToGrid) {
     grid->addNode({0, 0}, 1, 2);
+}
+
+TEST_F(GridTest, CanFindViablePairCountOfEmptyGrid) {
+    ASSERT_EQ(0, grid->countViablePairs());
+}
+
+TEST_F(GridTest, CanFindViablePairCountOfASinglePair) {
+    grid->addNode({0, 0}, 1, 999);
+    grid->addNode({0, 1}, 1, 999);
+    ASSERT_EQ(2, grid->countViablePairs());
+}
+
+TEST_F(GridTest, CanFindViablePairCountOfASinglePairWithAFatNode) {
+    grid->addNode({0, 0}, 10, 999);
+    grid->addNode({0, 1}, 10, 999);
+    grid->addNode({1, 0}, 99999, 1);
+    ASSERT_EQ(2, grid->countViablePairs());
 }
