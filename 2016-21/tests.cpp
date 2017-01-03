b77ee3069a11c580a86fb20532f0071cab33badf
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 18:13:44 -0600

    AoC 2016 Day 21 TDD

--- 2016-21/tests.cpp
+++ 2016-21/tests.cpp
@@ -8,7 +8,17 @@ namespace AoC {
     class Scrambler {
     public:
         Scrambler(std::string input) : input(input) {
+        }
 
+        void scramble(std::string command) {
+            std::stringstream stream(command);
+            std::string commandClass;
+            stream >> commandClass;
+            if ("swap" == commandClass) {
+                scrambleSwap(stream);
+            } else {
+                assert(false);
+            }
         }
 
         std::string result() {
@@ -16,13 +26,32 @@ namespace AoC {
         }
 
     private:
+        void scrambleSwap(std::istream& stream) {
+            std::string swapType;
+            stream >> swapType;
+            if ("position" == swapType) {
+                scrambleSwapPosition(stream);
+            } else {
+                assert(false);
+            }
+        }
+
+        void scrambleSwapPosition(std::istream& stream) {
+            int firstIndex;
+            std::string junk;
+            int secondIndex;
+            stream >> firstIndex >> junk >> junk >> secondIndex;
+            std::swap(input.at(firstIndex), input.at(secondIndex));
+        }
+
         std::string input;
     };
 }
 
 using namespace AoC;
 
-TEST(ScramblerTest, CanCreateAScramblerAndGetAResult) {
-    Scrambler scrambler("foo");
-    ASSERT_EQ("foo", scrambler.result());
+TEST(ScramblerTest, CanRunTheSampleTransormations) {
+    Scrambler scrambler("abcde");
+    scrambler.scramble("swap position 4 with position 0");
+    ASSERT_EQ("ebcda", scrambler.result());
 }
