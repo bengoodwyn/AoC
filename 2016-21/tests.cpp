52758a51b098c9ade6c84e1404e255c44e2d8c13
Ben Goodwyn <ben@clouddirective.com>
2017-01-02 18:06:22 -0600

    AoC 2016 Day 21 TDD

--- 2016-21/tests.cpp
+++ 2016-21/tests.cpp
@@ -7,14 +7,22 @@
 namespace AoC {
     class Scrambler {
     public:
-        Scrambler(std::string input) {
+        Scrambler(std::string input) : input(input) {
 
         }
+
+        std::string result() {
+            return input;
+        }
+
+    private:
+        std::string input;
     };
 }
 
 using namespace AoC;
 
-TEST(ScramblerTest, CanCreateAScrambler) {
+TEST(ScramblerTest, CanCreateAScramblerAndGetAResult) {
     Scrambler scrambler("foo");
+    ASSERT_EQ("foo", scrambler.result());
 }
